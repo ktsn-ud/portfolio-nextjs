47c9e0700687e88b6666ce0d22eb9bb78cfce3a8
ktsn-ud <153289885+ktsn-ud@users.noreply.github.com>
2025-12-06 10:56:20 +0900

    feat: Aboutページの内容を更新
    - Philosophyコンポーネントを追加
    - ProfileTableコンポーネントを追加
    - プロフィール情報を詳細に更新

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -1,9 +1,86 @@
 import Heading from "@/components/heading";
+import { FaArrowAltCircleDown } from "react-icons/fa";
 
 export default function Page() {
   return (
     <div>
       <Heading labelJa="自己紹介" labelEn="About" />
+      <Philosophy />
+      <ProfileTable />
     </div>
+  );
+}
+
+function Philosophy() {
+  const copyStyle =
+    "block w-fit text-2xl lg:text-4xl text-gray-50 bg-text my-2 lg:my-4";
+  return (
+    <div className="flex flex-col lg:flex-row section-center justify-center items-center">
+      <div className="text-2xl font-bold my-2">Philosophy</div>
+      <div className="lg:mx-15 font-bold">
+        <span className={copyStyle}>課題を自分ごと化し、</span>
+        <span className={copyStyle}>チームと共に価値を</span>
+        <span className={copyStyle}>創造するエンジニアへ</span>
+      </div>
+    </div>
+  );
+}
+
+function ProfileTable() {
+  const tableRowStyle = "border-b border-gray-300";
+  return (
+    <table className="table-auto w-full border-collapse border-gray-300 mt-8 text-sm lg:text-base">
+      <tbody className="border-t border-gray-300">
+        <tr className={tableRowStyle}>
+          <td className="p-3 font-bold">ニックネーム</td>
+          <td className="p-3">
+            きつねうどん（@ktsn-ud）
+            <span className="text-xs">※受験生時代にたくさん食べたから</span>
+          </td>
+        </tr>
+        <tr className={tableRowStyle}>
+          <td className="p-3 font-bold">所属</td>
+          <td className="p-3">
+            千葉大学 理学部 数学・情報数理学科 情報数理学コース
+          </td>
+        </tr>
+        <tr className={tableRowStyle}>
+          <td className="p-3 font-bold">卒業年度</td>
+          <td className="p-3">2027年3月 卒業見込み</td>
+        </tr>
+        <tr className={tableRowStyle}>
+          <td className="p-3 font-bold">スキル</td>
+          <td className="p-3 lg:flex">
+            <div>Python / React など</div>
+          </td>
+        </tr>
+        <tr className={tableRowStyle}>
+          <td className="p-3 font-bold">現在の関心分野</td>
+          <td className="p-3">Webアプリ開発、アルゴリズム</td>
+        </tr>
+        <tr className={tableRowStyle}>
+          <td className="p-3 font-bold">強み</td>
+          <td className="p-3 lg:flex items-end">
+            <ul>
+              <li>論理的思考力と課題解決力</li>
+              <li>高い自走力と学習意欲</li>
+              <li>チーム貢献力と支援型リーダーシップ</li>
+            </ul>
+          </td>
+        </tr>
+        <tr className={tableRowStyle}>
+          <td className="p-3 font-bold">趣味</td>
+          <td className="p-3">
+            競技プログラミング
+            <br />
+            アニメを見ること
+            <br />
+            <span className="text-xs">
+              ※キャラクターの心の動きに共感したり、関係性の変化を楽しむのが好きです。
+            </span>
+          </td>
+        </tr>
+      </tbody>
+    </table>
   );
 }
