807cb1e187db921050a697ff6fa67fc0ab84a0ae
ktsn-ud <153289885+ktsn-ud@users.noreply.github.com>
2025-12-06 12:50:59 +0900

    feat: お問い合わせページを追加し、Googleフォームへのリンクを設置
    - app/contact/page.tsx: お問い合わせページを追加、Googleフォームへのリンクとボタンを設置
    - app/works/page.tsx: 現在準備中のメッセージを追加

--- app/contact/page.tsx
+++ app/contact/page.tsx
@@ -1,9 +1,34 @@
 import Heading from "@/components/heading";
+import { SiGoogleforms } from "react-icons/si";
 
 export default function Page() {
   return (
     <div>
       <Heading labelJa="お問い合わせ" labelEn="Contact" />
+      <p>本サイトを御覧いただきありがとうございました。</p>
+      <p>
+        ポートフォリオに関するご意見・ご質問・ご連絡がございましたら、以下のリンクからお問い合わせください。
+      </p>
+      <ContactButton />
+    </div>
+  );
+}
+
+function ContactButton() {
+  return (
+    <div className="my-4">
+      <a
+        className="w-80 h-15 rounded-full bg-gray-800 text-[white] flex items-center justify-center mx-auto mb-2 lg:hover:scale-105 lg:transition transition-500"
+        href="https://docs.google.com/forms/d/e/1FAIpQLSdiqMqA88Z5WZsyke0uXx6jr0zhaawLTajeRGuPCY8OblCzJg/viewform?usp=dialog"
+        target="_blank"
+        rel="noopener noreferrer"
+      >
+        <SiGoogleforms className="w-8 h-8 mr-2"></SiGoogleforms>
+        お問い合わせフォーム
+      </a>
+      <p className="text-center text-gray-500 text-sm">
+        ※Googleフォームに遷移します
+      </p>
     </div>
   );
 }
--- app/works/page.tsx
+++ app/works/page.tsx
@@ -4,6 +4,7 @@ export default function Page() {
   return (
     <div>
       <Heading labelJa="作品" labelEn="Works" />
+      <p>現在準備中...</p>
     </div>
   );
 }
